b731db491a1ce806ea8f04e482e5401a1c25e296
Tobiichi <tobiichi.ma@gmail.com>
2025-10-10 03:04:24 -0500

    Añadir ejercicio 1

---
+++ Ejercicio-1/Ejercicio1-ValidarExpMat/Program.cs
@@ -0,0 +1,63 @@
+﻿
+Pila<char> pila;
+bool correcta = true;
+string cadena = "";
+Console.WriteLine("Ingrese una expresion matematica:");
+cadena = Console.ReadLine();
+pila = new Pila<char>(cadena.Length);
+
+for (int i = 0; i < cadena.Length; i++)
+{
+    if (cadena[i] == '(' || cadena[i] == '[' || cadena[i] == '{')
+    {
+        pila.Push(cadena[i]);
+    }
+    else if (!pila.Vacia)
+    {
+        if (cadena[i] == ')' || cadena[i] == ']' || cadena[i] == '}')
+        {
+            if (cadena[i] == ')')
+            {
+                if (pila.LeerUltimo() == '(')
+                    pila.Pop();
+                else
+                {
+                    Console.WriteLine("La expresión no está bien escrita");
+                    i = cadena.Length;
+                    correcta = false;
+                }
+            }
+            else if (cadena[i] == ']')
+            {
+                if (pila.LeerUltimo() == '[')
+                    pila.Pop();
+                else
+                {
+                    Console.WriteLine("La expresión no está bien escrita");
+                    i = cadena.Length;
+                    correcta = false;
+                }
+            }
+            else if (pila.LeerUltimo() == '{')
+            {
+                pila.Pop();
+            }
+            else
+            {
+                Console.WriteLine("La expresión no está bien escrita");
+                i = cadena.Length;
+                correcta = false;
+            }
+        }
+    }
+}
+if (!pila.Vacia)
+{
+    correcta = false;
+    Console.WriteLine("La expresión no está bien escrita");
+}
+if (correcta)
+{
+    Console.WriteLine("La expresión está bien escrita");
+}
+Console.ReadKey();
---
+++ Ejercicio-1/Ejercicio1-ValidarExpMat/pila.cs
@@ -0,0 +1,53 @@
+public class Pila<Tipo>
+{
+    private int Max;
+    private int top;
+    private Tipo[] Arreglo;
+
+    public Pila(int tamaño)
+    {
+        Max = tamaño - 1;
+        top = -1;
+        Arreglo = new Tipo[Max];
+    }
+    public bool Vacia
+    {
+        get
+        {
+            if (top == -1)
+                return true;
+            else
+                return false;
+        }
+    }
+    public bool Llena
+    {
+        get { return top == Max; }
+    }
+    public void Push(Tipo dato)
+    {
+        if (!Llena)
+        {
+            top++;
+            Arreglo[top] = dato;
+        }
+    }
+    public Tipo Pop()
+    {
+        if (!Vacia)
+        {
+            Tipo dato = Arreglo[top];
+            top--;
+            return dato;
+        }
+        else
+            throw new Exception("Pila vacia");
+    }
+    public Tipo LeerUltimo()
+    {
+        if (!Vacia)
+            return Arreglo[top];
+        else
+            throw new Exception("Pila vacia");
+    }
+}
