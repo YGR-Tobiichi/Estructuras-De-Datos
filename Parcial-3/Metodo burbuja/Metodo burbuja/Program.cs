d39e8fae49113ece9584ee18327731c68c1336d1
Tobiichi <tobiichi.ma@gmail.com>
2025-11-22 16:17:07 -0600

    Agregar ejercicio del método burbuja

---
+++ Parcial-3/Metodo burbuja/Metodo burbuja/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] arreglo = new int[6];
+bool Cambio = true;
+Console.WriteLine("Ingrese 6 números enteros:");
+for (int i = 0; i < arreglo.Length; i++)
+{
+    Console.Write("Número {i + 1}: ");
+    arreglo[i] = int.Parse(Console.ReadLine());
+}
+
+// Método de ordenamiento burbuja
+for (int i = 0; i < arreglo.Length - 1; i++)
+{
+    if (Cambio)
+        Cambio = false;
+    else
+        i = arreglo.Length;
+    for (int j = 0; j < arreglo.Length - 1; j++)
+    {
+        if (arreglo[j] > arreglo[j + 1])
+        {
+            int temporal = arreglo[j];
+            arreglo[j] = arreglo[j + 1];
+            arreglo[j + 1] = temporal;
+            Cambio = true;
+        }
+    }
+}
+Console.WriteLine("Arreglo ordenado:");
+for (int i = 0; i < arreglo.Length; i++)
+{
+    Console.WriteLine(arreglo[i]);
+}
+Console.ReadKey();
